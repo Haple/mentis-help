9030821ba6bc9d386e9f4d7368931d1270d317a5
GuilhermeAlr <guilhermealmendro@gmail.com>
2021-11-27 20:04:35 -0300

    Chamada de video

--- includes/cadastrar_horario_inc.php
+++ includes/cadastrar_horario_inc.php
@@ -5,12 +5,11 @@ if (isset($_POST["cadastrar_horario"])) {
     $dia = $_POST["dia"];
     $hora = $_POST["hora"];
     $refID = $_POST["id"];
-    $link = $_POST["link"];
     
     require_once 'db_inc.php';
     require_once 'functions_horario_inc.php';
 
-    criarHorario($conn, $refID, $dia, $hora, $link);
+    criarHorario($conn, $refID, $dia, $hora);
 
 }
 else{
