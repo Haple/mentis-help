318edd519ba9dc80e9059bcb4cf00c804bb737f5
haple <aleph.oliveira@etec.sp.gov.br>
2021-11-28 21:43:46 -0300

    Fix interpolation issue

--- pages/dashboard_paciente/DBchats_paciente.php
+++ pages/dashboard_paciente/DBchats_paciente.php
@@ -1,7 +1,7 @@
 <?php
-    session_start();
-    include_once('../../includes/db_inc.php');
-    $currentUser = $_SESSION['idpaciente'];
+session_start();
+include_once('../../includes/db_inc.php');
+$currentUser = $_SESSION['idpaciente'];
 ?>
 <!doctype html>
 <html lang="pt-br">
@@ -13,8 +13,7 @@
     <title>Mentis</title>
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500&display=swap" rel="stylesheet">
     <!-- My CSS -->
@@ -34,9 +33,7 @@
                     <a class="navbar-brand dash-title" href="../../index.php">
                         <div class="logo me-3">Mentis<span class="logo-ponto">.</span></div>
                     </a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -47,15 +44,13 @@
                                     Paciente</a>
                             </li>
                             <li class="nav-item me-2">
-                                <a class="nav-link active" aria-current="page" href="DBchats_paciente.php"><i
-                                        class="bi bi-chat-left"></i> Consulta</a>
+                                <a class="nav-link active" aria-current="page" href="DBchats_paciente.php"><i class="bi bi-chat-left"></i> Consulta</a>
                             </li>
                             <li class="nav-item me-2">
                                 <a class="nav-link" href="DBperfil_paciente.php"><i class="bi bi-person"></i> Perfil</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link text-danger" href="../../includes/logout_inc.php"><i
-                                        class="bi bi-power"></i> Sair</a>
+                                <a class="nav-link text-danger" href="../../includes/logout_inc.php"><i class="bi bi-power"></i> Sair</a>
                             </li>
                         </ul>
                     </div>
@@ -78,26 +73,25 @@
                 </thead>
                 <tbody>
                     <?php
-                        $sql = "select * from consulta where IDPaciente_c = '$currentUser';";
-                        $rs = mysqli_query($conn, $sql);
-                        while($row = mysqli_fetch_assoc($rs)){
-                        ?>
-                    <tr>
-                        <td><?=  $row["nomePsicologo_c"] ?></td>
-                        <td><?=  $row["emailPsicologo_c"] ?></td>
-                        <td><?=  $row["dia_c"] ?></td>
-                        <td><?=  $row["hora_c"] ?></td>
-                    </tr>
+                    $sql = "select * from consulta where IDPaciente_c = '" . $currentUser . "';";
+                    $rs = mysqli_query($conn, $sql);
+                    while ($row = mysqli_fetch_assoc($rs)) {
+                    ?>
+                        <tr>
+                            <td><?= $row["nomePsicologo_c"] ?></td>
+                            <td><?= $row["emailPsicologo_c"] ?></td>
+                            <td><?= $row["dia_c"] ?></td>
+                            <td><?= $row["hora_c"] ?></td>
+                        </tr>
                     <?php
-                        }
+                    }
                     ?>
                 </tbody>
             </table>
         </div>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous">
     </script>
 </body>
 
