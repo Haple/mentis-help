bad3291539ffe1728b80b3e4e2175db46f5b58f9
haple <aleph.oliveira@etec.sp.gov.br>
2021-11-28 21:31:31 -0300

    fix query

--- pages/dashboard_paciente/dashboard_paciente.php
+++ pages/dashboard_paciente/dashboard_paciente.php
@@ -1,21 +1,20 @@
 <?php
-    session_start();
-    include_once('../../includes/db_inc.php');
-    $currentUser = $_SESSION['idpaciente'];
-                    if(isset($_POST['agendar_hhhorario'])){
-                        $novoid = $_POST['id'];
-                        $novonome = $_POST['nome'];
-                
-                        $sql="UPDATE adicionar_horario SET Ref_IDPaciente = '$novoid', NomePaciente = '$novonome' WHERE IDadd_horario = id_upd";
-                        $result=mysqli_query($conn, $sql);
-                        if($result){
-                                echo "<div class='alert alert-success' role='alert'>Perfil atualizado!</div>";
-                        }
-                        else{
-                            echo "<div class='alert alert-success' role='alert'>Não foi possível atualizar!</div>";
-                            die(mysqli_error($conn));
-                        }
-                    }
+session_start();
+include_once('../../includes/db_inc.php');
+$currentUser = $_SESSION['idpaciente'];
+if (isset($_POST['agendar_hhhorario'])) {
+    $novoid = $_POST['id'];
+    $novonome = $_POST['nome'];
+
+    $sql = "UPDATE adicionar_horario SET Ref_IDPaciente = '$novoid', NomePaciente = '$novonome' WHERE IDadd_horario = id_upd";
+    $result = mysqli_query($conn, $sql);
+    if ($result) {
+        echo "<div class='alert alert-success' role='alert'>Perfil atualizado!</div>";
+    } else {
+        echo "<div class='alert alert-success' role='alert'>Não foi possível atualizar!</div>";
+        die(mysqli_error($conn));
+    }
+}
 ?>
 <!doctype html>
 <html lang="pt-br">
@@ -27,8 +26,7 @@
     <title>Mentis</title>
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500&display=swap" rel="stylesheet">
     <!-- My CSS -->
@@ -48,16 +46,13 @@
                     <a class="navbar-brand dash-title" href="../../index.php">
                         <div class="logo me-3">Mentis<span class="logo-ponto">.</span></div>
                     </a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                             <li class="nav-item me-2">
-                                <a class="nav-link active" aria-current="page" href="dashboard_paciente.php"><i
-                                        class="bi bi-house"></i>
+                                <a class="nav-link active" aria-current="page" href="dashboard_paciente.php"><i class="bi bi-house"></i>
                                     Área do
                                     Paciente</a>
                             </li>
@@ -69,8 +64,7 @@
                                 <a class="nav-link" href="DBperfil_paciente.php"><i class="bi bi-person"></i> Perfil</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link text-danger" href="../../includes/logout_inc.php"><i
-                                        class="bi bi-power"></i> Sair</a>
+                                <a class="nav-link text-danger" href="../../includes/logout_inc.php"><i class="bi bi-power"></i> Sair</a>
                             </li>
                         </ul>
                     </div>
@@ -96,25 +90,34 @@
                 </thead>
                 <tbody>
                     <?php
-                        $sql = "select (YEAR(CURDATE()) - YEAR(nascimentoPsicologo)) - (right(CURDATE(),5) < right(nascimentoPsicologo,5)) as idade, i.nomePsicologo, i.emailPsicologo, i.sexoPsicologo, i.nascimentoPsicologo, h.dia, h.hora, h.IDadd_horario from psicologo as i JOIN adicionar_horario as h on i.IDPsicologo = h.Ref_IDPsicologo WHERE disponibilidade = 'Disponível' ORDER by dia;";
-                        $rs = mysqli_query($conn, $sql);
-                        while($row = mysqli_fetch_assoc($rs)){
-                        ?>
-                    <tr>
-                        <td><?=  $row["IDadd_horario"] ?></td>
-                        <td><?=  $row["dia"] ?></td>
-                        <td><?=  $row["hora"] ?></td>
-                        <td><?=  $row["nomePsicologo"] ?></td>
-                        <td><?=  $row["emailPsicologo"] ?></td>
-                        <td><?=  $row["sexoPsicologo"] ?></td>
-                        <td><?=  $row["idade"] ?></td>
-                        <td><a name="upd_horario" type="submit"
-                                href="agendar_paciente.php?id_agendar=<?php echo $row['IDadd_horario'];?>"
-                                class="btn section-btn2 fw-bold"> Agendar</a>
-                        </td>
-                    </tr>
+
+                    error_log("AHA!");
+
+                    $sql = "select (YEAR(CURDATE()) - YEAR(nascimentoPsicologo)) - (right(CURDATE(),5) < right(nascimentoPsicologo,5)) as idade, i.nomePsicologo, i.emailPsicologo, i.sexoPsicologo, i.nascimentoPsicologo, h.dia, h.hora, h.IDadd_horario from psicologo as i JOIN adicionar_horario as h on i.IDPsicologo = h.Ref_IDPsicologo WHERE disponibilidade = 'Disponivel' ORDER by dia;";
+                    // $sql = "select * from psicologo as i JOIN adicionar_horario as h on i.IDPsicologo = h.Ref_IDPsicologo WHERE disponibilidade = 'Disponível' ORDER by dia;";
+                    $rs = mysqli_query($conn, $sql);
+                    if (mysqli_num_rows($rs) > 0) {
+                        error_log("AHA! There is something here!");
+                    } else {
+                        error_log("OH NO! There is nothing here!");
+                    }
+
+                    while ($row = mysqli_fetch_assoc($rs)) {
+
+                    ?>
+                        <tr>
+                            <td><?= $row["IDadd_horario"] ?></td>
+                            <td><?= $row["dia"] ?></td>
+                            <td><?= $row["hora"] ?></td>
+                            <td><?= $row["nomePsicologo"] ?></td>
+                            <td><?= $row["emailPsicologo"] ?></td>
+                            <td><?= $row["sexoPsicologo"] ?></td>
+                            <td><?= $row["idade"] ?></td>
+                            <td><a name="upd_horario" type="submit" href="agendar_paciente.php?id_agendar=<?php echo $row['IDadd_horario']; ?>" class="btn section-btn2 fw-bold"> Agendar</a>
+                            </td>
+                        </tr>
                     <?php
-                        }
+                    }
                     ?>
                 </tbody>
             </table>
@@ -218,55 +221,50 @@
         </div>
     </div>-->
     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <?php           
-            include_once '../../includes/db_inc.php';
-            $currentUser = $_SESSION['idpaciente'];
-            $sql = "SELECT * FROM paciente WHERE IDPaciente = $currentUser";
-            $gotResults = mysqli_query($conn, $sql);
-                if($gotResults){
-                    if(mysqli_num_rows($gotResults)>0){
-                    while($row = mysqli_fetch_array($gotResults)){
-                        //print_r($row);
+        <?php
+        include_once '../../includes/db_inc.php';
+        $currentUser = $_SESSION['idpaciente'];
+        $sql = "SELECT * FROM paciente WHERE IDPaciente = $currentUser";
+        $gotResults = mysqli_query($conn, $sql);
+        if ($gotResults) {
+            if (mysqli_num_rows($gotResults) > 0) {
+                while ($row = mysqli_fetch_array($gotResults)) {
+                    //print_r($row);
         ?>
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <p>Deseja agendar uma consulta neste horário?</p>
-                    <form method="post">
-                        <div class="mb-3">
-                            <div class="form-floating">
-                                <input type="hidden" name="id" value="<?php echo $row['IDPaciente']; ?>"
-                                    class="form-control" id="floatingInputGrid"
-                                    placeholder="insira o dia de atendimento" required>
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <p>Deseja agendar uma consulta neste horário?</p>
+                                <form method="post">
+                                    <div class="mb-3">
+                                        <div class="form-floating">
+                                            <input type="hidden" name="id" value="<?php echo $row['IDPaciente']; ?>" class="form-control" id="floatingInputGrid" placeholder="insira o dia de atendimento" required>
+                                        </div>
+                                    </div>
+                                    <div class="mb-3">
+                                        <div class="form-floating">
+                                            <input type="hidden" name="nome" value="<?php echo $row['nomePaciente']; ?>" class="form-control" id="floatingInputGrid" placeholder="insira o dia de atendimento" required>
+                                        </div>
+                                    </div>
+                                    <button type="submit" name="agendar_horario" class="btn section-btn2 me-2">Sim</button>
+                                    <button type="button" class="btn btn-outline-danger" data-bs-dismiss="modal">Não</button>
+                                </form>
                             </div>
                         </div>
-                        <div class="mb-3">
-                            <div class="form-floating">
-                                <input type="hidden" name="nome" value="<?php echo $row['nomePaciente']; ?>"
-                                    class="form-control" id="floatingInputGrid"
-                                    placeholder="insira o dia de atendimento" required>
-                            </div>
-                        </div>
-                        <button type="submit" name="agendar_horario" class="btn section-btn2 me-2">Sim</button>
-                        <button type="button" class="btn btn-outline-danger" data-bs-dismiss="modal">Não</button>
-                    </form>
-                </div>
-            </div>
-        </div>
+                    </div>
     </div>
-    <?php
-                                }
-                            }
-                        }
-                    ?>
+<?php
+                }
+            }
+        }
+?>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous">
-    </script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous">
+</script>
 </body>
 
 </html>
